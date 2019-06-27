ed6d3673495cfd27dabc6b669689915da77e5b6f
iFun <bill.guocheng@gmail.com>
2019-06-27 14:58:18 -0400

    updated

--- components/d2l-completion-requirement.js
+++ components/d2l-completion-requirement.js
@@ -1,7 +1,5 @@
 import { CompletionStatusMixin } from '../utility/completion-status-mixin.js';
 import 'd2l-icons/d2l-icons.js';
-import 'd2l-status-indicator/d2l-status-indicator.js';
-import d2lIntl from 'd2l-intl';
 import 'd2l-colors/d2l-colors.js';
 import 'd2l-typography/d2l-typography.js';
 import { html } from '@polymer/polymer/lib/utils/html-tag.js';
@@ -19,51 +17,23 @@ class D2LCompletionRequirement extends CompletionStatusMixin() {
 				display: inline;
 				@apply --d2l-body-small-text;
 			}
-
-			#container
-			{
-				 display: flex;
-			}
-			d2l-status-indicator{
-				margin-right: 4px;
-			}
-
 		</style>
-
-		<div id="container">
-			<template is="dom-if" if="[[isOverDue]]">
-				<d2l-status-indicator state="alert" text="overdue"></d2l-status-indicator>
-			</template>
-
-			<div id="dueDate">[[dueDate]]</div>
-
-			<template is="dom-if" if="[[shouldShowBullet]]">
-				<d2l-icon icon="d2l-tier1:bullet"></d2l-icon>
-			</template>
-			<template is="dom-if" if="[[isExempt]]">
-				<div class="exempt">
-					[[localize('exempt')]]
-				</div>
-			</template>
-			<template is="dom-if" if="[[isOptional]]">
-				<div class="optional">
-					[[localize('optional')]]
-				</div>
-			</template>
-
-		</div>
-
-
+		<template is="dom-if" if="[[isExempt]]">
+			<div class="exempt">
+				[[localize('exempt')]]
+			</div>
+		</template>
+		<template is="dom-if" if="[[isOptional]]">
+			<div class="optional">
+				[[localize('optional')]]
+			</div>
+		</template>
 `;
 	}
 
 	static get is() {
 		return 'd2l-completion-requirement';
 	}
-	static get behaviors() {
-		D2L.PolymerBehaviors.LocalizeBehavior;
-	}
-
 	static get properties() {
 		return {
 			completionRequirement: {
@@ -76,18 +46,6 @@ class D2LCompletionRequirement extends CompletionStatusMixin() {
 			},
 			isOptional: {
 				type: Boolean
-			},
-			dueDate: {
-				type: String,
-				computed: 'getFormatedDate(entity)'
-			},
-			isOverDue:{
-				type: Boolean,
-				computed: '_setOverDue(entity,isOptional,isExempt)',
-			},
-			shouldShowBullet: {
-				type: Boolean,
-				computed: '_showBullet(dueDate,isOptional,isExempt)'
 			}
 		};
 	}
@@ -104,40 +62,6 @@ class D2LCompletionRequirement extends CompletionStatusMixin() {
 				this.isExempt = false;
 				this.isOptional = false;
 		}
-	}
-
-	_showBullet(dueDate, optional, exempt) {
-		return dueDate && (optional || exempt);
-	}
-
-	_setOverDue(entity, optional, exempt) {
-		if (entity && entity.properties && entity.properties.overDue) {
-			if (optional === false && exempt === false) {
-				this.$$('#dueDate').style = 'color: var(--d2l-color-cinnabar)';
-			}
-			return true;
-		}
-		return false;
-	}
-
-	getFormatedDate(entity) {
-		if (!entity || !entity.properties || !entity.properties.dueDate) {
-			return '';
-		}
-		const year = entity.properties.dueDate.Year;
-		const day = entity.properties.dueDate.Day;
-		const month = entity.properties.dueDate.Month - 1;
-		const formatter = new d2lIntl.DateTimeFormat(this.language, {
-			format: 'monthDay'
-		});
-		const longResult = formatter.formatDate(
-			new Date(year, month, day),
-		);
-
-		// d2lIntl short month format is not working, also it does not have short month and date formate
-		const result = longResult.replace(longResult.substring(0, longResult.indexOf(' ')),
-			longResult.substring(0, 3));
-		return this.localize('due') + ' ' + result;
 	}
 }
 customElements.define(D2LCompletionRequirement.is, D2LCompletionRequirement);
